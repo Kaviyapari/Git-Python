81b45201ec9a2097cc775378d8293519f3a571f9
Kaviyapari <kaviyapari1916@gmail.com>
2025-02-05 13:47:56 +0530

    Update menu .app.py

--- menu .app.py
+++ menu .app.py
@@ -2,18 +2,19 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-menu = [
+Dinnermenu = [
     {"Name": "Chicken Noodles", "Price": 180},
     {"Name": "Chicken Rice", "Price": 150},
-    {"Name": "Naan with Chicken Tikka Masala", "Price": 200}
+    {"Name": "Naan with Chicken Tikka Masala", "Price": 200},
+    {"Name": "Boratta","Price": 40}
 ]
 
 @app.route('/data', methods=['GET', 'POST'])
 def handle_data():
     if request.method == 'POST':
         data = request.get_json()
-        menu.append(data)
-    return jsonify(menu)
+        Dinnermenu.append(data)
+    return jsonify(Dinnermenu)
 
 if __name__ == '__main__':
     app.run(debug=True)
